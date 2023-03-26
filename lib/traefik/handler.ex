f6f019e5768f44330115cc62aba2a437612f4dcf
rosaainz <al2173040136@azc.uam.mx>
2023-03-26 16:12:42 -0600

    Pattern matching en funciones nombradas (varias funciones del mismo nombre, mismo numero de arg pero diferentes valores)

--- lib/traefik/handler.ex
+++ lib/traefik/handler.ex
@@ -1,44 +1,62 @@
 defmodule Traefik.Handler do
-	def handle(request) do
-		request
-		|> parse()
-		|> route()
-		|> format_response()
-	end
+  def handle(request) do
+    request
+    |> parse()
+    |> route()
+    |> format_response()
+  end
 
-	def parse(request) do
-		[method, path, _] =
-		request
-		|> String.split("\n")
-		|> List.first()
-		|> String.split(" ")
+  def parse(request) do
+    [method, path, _] =
+      request
+      |> String.split("\n")
+      |> List.first()
+      |> String.split(" ")
 
-		%{method: method, path: path, response: ""}
-	end
+    %{method: method, path: path, response: ""}
+  end
 
-	def route(conn)do
-		 %{conn | response: "Hello 😛"}
-	end
+  def route(conn) do
+    route(conn, conn.method, conn.path)
+  end
 
-	def format_response(conn) do
-		"""
-		    HTTP/1.1 200 OK
-		    Content-Type: text/html
-		    Content-Lenght: #{String.length(conn.response)}
-		    @neodevelop, @makingdevs, @elixirlang
-		#{conn.response}
+  def route(conn, "GET", "/request1🤖") do
+    %{conn | response: "Hello 🤖"}
+  end
 
-		"""
-	end
+  def route(conn, "GET", "/request2🌹") do
+    %{conn | response: "Hola 🌹"}
+  end
+
+  def format_response(conn) do
+    """
+    HTTP/1.1 200 OK
+    Content-Type: text/html
+    Content-Lenght: #{String.length(conn.response)}
+
+    #{conn.response}
+    """
+  end
 end
 
-	request = """
-		GET /developers HTTP/1.1
-		Host: makingdevs.com
-		User-Agent: MyBrowser/0.1
-		Accept: */*
+request = """
+GET /request1🤖 HTTP/1.1
+Host: makingdevs.com
+User-Agent: MyBrowser/0.1
+Accept: */*
 
-		"""
+"""
 
-	response = Traefik.Handler.handle(request)
-	IO.puts(response)
+response = Traefik.Handler.handle(request)
+IO.puts(response)
+
+request = """
+GET /request2🌹 HTTP/1.1
+Host: makingdevs.com
+User-Agent: MyBrowser/0.1
+Accept: */*
+
+"""
+
+response = Traefik.Handler.handle(request)
+IO.puts(response)
